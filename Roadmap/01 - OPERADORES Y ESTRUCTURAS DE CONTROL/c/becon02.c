efe9c8853a49975480d83a25d93552a5df02c3e5
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-01-16 01:50:01 +0100

    #01 - Language C || In progress 2

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/c/becon02.c
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/c/becon02.c
@@ -1,51 +1,90 @@
 #include <stdio.h>
 
-void operadores();
-void condicionales();
-void iterativas();
-void desafio();
+void operators();
+void conditionals();
+void iteratives();
+void challenge();
 
 int main(void)
 {
-    operadores();
-    condicionales();
-    iterativas();
-    desafio();
+    operators();
+    conditionals();
+    iteratives();
+    challenge();
 
     return(0);
 }
 
-/* OPERADORES
-Este programa (código) demuestra los operadores más comunes en C, clasificados en:
-- Aritméticos: para realizar cálculos matemáticos
-- Comparación: para comparar valores y generar resultados booleanos
-- Lógicos: para realizar operaciones con condiciones (AND, OR, NOT)
-- Asignación: para modificar el valor de una variable
-- Bits: para trabajar con operaciones a nivel de bits (AND, OR, desplazamientos) 
+/* Operators
+This program showcase the most common operators in C, sorted by:
+- Arithmetic: To do mathematic calculations
+- Comparison: To compare values and generate boleans
+- Logic: To make operations with conditions (AND, OR, NOT)
+- Assignment: To modify the value of a variable
+- Bits: To work with operations at bit level (AND, OR, displacement) 
 */
-void operadores()
+void operators()
 {
+    // Global variables for function 
     int a = 5;
-    int b = 10; //Variables globales de la function
+    int b = 10; 
+    int c = 3;
 
-    //Operadores Aritmeticos
-    printf("--- Operadores Aritmeticos ---\n");
+    // Arithmetic Operators
+    printf("--- Arithmetic Operators ---\n");
     {
-        printf("Suma: %d + %d = %d", a, b, a + b);
+        printf("Addition: %d + %d = %d\n", a, b, a + b);
+        printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+        printf("Division: %d / %d = %f\n", a, b, (float)a / b); //Division with 0 gives ERROR inf
+        printf("Product: %d * %d = %d\n", a, b, a * b);
+        printf("Modulus: %d %% %d = %d\n", a, c, a % c); //Use %% in printf() to print the symbol 
+        printf("Negative symbol: %d -> %d\n", a, -a);
+        
+        a++; //Increment
+        printf("Increment: Before a = 5; After a++ = %d\n", a); //Show value before and after increment
+
+        b--; //Decrement
+        printf("Decrement Before b = 10; After b-- = %d\n\n", b); //Show value before and after decrement
+    }
+
+    //Comparison Operators
+    printf("--- Comparison Operators ---\n");
+    printf(" (0) = False; (1) = True\n\n"); //In C theres no Bool type, everything except 0 or '\0' is True
+    {
+        printf("Equal: (%d == %d) = %d\n", a, b, a == b);
+        printf("Not equal: (%d != %d) = %d\n", a, b, a != b);
+        printf("Greater than: (%d > %d) = %d\n", a, b, a > b);
+        printf("Smaller than: (%d < %d) = %d\n", a, b, a < b);
+        printf("Greater than || Equal to: (%d >= %d) = %d\n", a, b, a >= b);
+        printf("Smaller than || Equal to: (%d <= %d) = %d\n", a, b, a <= b);
+    }
+
+    //Logic Opetators
+    printf("--- Logic Operators ---\n");
+    printf(" (0) = False; (1) = True\n\n");
+    {
+        //AND (&&) -> True if both comparison are True
+        printf("AND -> Both are True?: (%d < %d && %d > %d) = %d\n", a, b, b, a , (a < b && b > a));
+
+        //OR (||) -> True if one of the comparison is True
+        printf("OR -> Is one True?:(%d == %d || %d <= %d) = %d\n", a, c, c, b, (a == c || c <= b));
+    
+        //NOT (!) -> Reverses the result
+        printf("NOT -> Reverse the result (6 == 3) = 0; !(%d == %d) = %d", a, c, !(a == c));
     }
 }
 
-void condicionales()
+void conditionals()
 {
 
 }
 
-void iterativas()
+void iteratives()
 {
 
 }
 
-void desafio()
+void challenge()
 {
 
 }
